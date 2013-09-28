32db122b2861cd392e32733ff77a0079ea7db340
Alvin_Zhang <mosoft521@gmail.com>
2013-09-28 09:20:06 +0800

    reformat

--- src/main/java/com/himtech/datasource/Student.java
+++ src/main/java/com/himtech/datasource/Student.java
@@ -1,65 +1,54 @@
 package com.himtech.datasource;
 
-public class Student
-{
-	private String stuid;
-	
-	private String name;
-	
-	private String course;
-	
-	private String grade;
-	
-	public Student()
-	{
-		
-	}
-	
-	public Student(String id, String name, String course, String grade)
-	{
-		this.stuid = id;
-		this.name = name;
-		this.course = course;
-		this.grade = grade;
-	}
+public class Student {
+    private String stuid;
 
-	public String getStuid()
-	{
-		return stuid;
-	}
+    private String name;
 
-	public void setStuid(String stuid)
-	{
-		this.stuid = stuid;
-	}
+    private String course;
 
-	public String getName()
-	{
-		return name;
-	}
+    private String grade;
 
-	public void setName(String name)
-	{
-		this.name = name;
-	}
+    public Student() {
 
-	public String getCourse()
-	{
-		return course;
-	}
+    }
 
-	public void setCourse(String course)
-	{
-		this.course = course;
-	}
+    public Student(String id, String name, String course, String grade) {
+        this.stuid = id;
+        this.name = name;
+        this.course = course;
+        this.grade = grade;
+    }
 
-	public String getGrade()
-	{
-		return grade;
-	}
+    public String getStuid() {
+        return stuid;
+    }
 
-	public void setGrade(String grade)
-	{
-		this.grade = grade;
-	}
+    public void setStuid(String stuid) {
+        this.stuid = stuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
 }
--- src/main/java/com/himtech/report/JasperBean.java
+++ src/main/java/com/himtech/report/JasperBean.java
@@ -1,30 +1,28 @@
 package com.himtech.report;
 
 
+import com.himtech.datasource.StudentDataSource;
 import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.JRException;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.himtech.datasource.StudentDataSource;
-
 @Controller
 public class JasperBean {
 
-	private JRDataSource jrDatasource;
+    private JRDataSource jrDatasource;
 
-	public JasperBean() throws JRException {
-		StudentDataSource dsStudent =  new StudentDataSource();
-		jrDatasource = dsStudent.create(null);
-	}
+    public JasperBean() throws JRException {
+        StudentDataSource dsStudent = new StudentDataSource();
+        jrDatasource = dsStudent.create(null);
+    }
 
-	@RequestMapping(value = "/jrreport", method = RequestMethod.GET)
-	public String printWelcome(ModelMap model) {
-		model.addAttribute("datasource", jrDatasource);
-		model.addAttribute("format", "pdf");
-		return "multiViewReport";
-	}
+    @RequestMapping(value = "/jrreport", method = RequestMethod.GET)
+    public String printWelcome(ModelMap model) {
+        model.addAttribute("datasource", jrDatasource);
+        model.addAttribute("format", "pdf");
+        return "multiViewReport";
+    }
 }
